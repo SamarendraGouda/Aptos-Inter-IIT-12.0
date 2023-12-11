fad00f354741c2d4a7de0a6ea686fa69a2566382
Samarendra Gouda <samarendragouda@gmail.com>
2023-12-11 16:58:27 +0530

    fix: delete-old-backend

--- backend/src/utils/errorHandler.ts
+++
@@ -1,8 +0,0 @@
-export const handleError = (error: any, tag?: string) => {
-  const errorTag = `Error ${tag ? `: ${tag} ` : ""}`;
-  if (error instanceof Error) {
-    console.log(`${errorTag}: ${error.message}`);
-  } else {
-    console.log(`${errorTag}: Unknow Error Occured`);
-  }
-};
